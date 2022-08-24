d78495dee6ca058f409f67a6573117d876a0fd64
KonstantinSF <kosmos008@gmail.com>
2022-08-24 22:03:21 +0300

    factorial *2

--- Recursion1/Source.cpp
+++ Recursion1/Source.cpp
@@ -3,22 +3,25 @@ using namespace std;
 #define tab "\t"
 int Factorial(int n); 
 int factorial = 1;
+int FactorialIdeal(int n);
 int Pow(int base, int power);
+int Power(int base, int power);
 int powResult = 1; 
-void Fibonachi(int limitFibonachi);
-int numFibonachi = 0; 
+//void Fibonacсi(int n, int a=0, int b=1);
+//int numFibonachi = 0; 
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	/*int n; 
+	int n; 
 	cout << "Введите число для вычисления факториала:"; cin>>n;
-	cout << "Значение факториала равно: " << Factorial(n);*/
-	/*int base; int power; 
+	cout << "Значение факториала равно: " << Factorial(n) << endl; 
+	cout << "или другим способом: "<<FactorialIdeal(n) << endl;
+	int base; int power; 
 	cout << "Введите значения основания степени: "; cin >> base;
 	cout << "Введите значение показателя степени: "; cin >> power; 
-	cout << base << " в степени " << power << "=" << Pow(base, power);*/
-
+	cout << base << " в степени " << power << "=" << Pow(base, power) << endl;
+	cout << base << " в степени " << power << "=" << Power(base, power) << endl;
 }
 
 int Factorial(int n)
@@ -30,17 +33,25 @@ int Factorial(int n)
 	factorial*= n; 
 	Factorial(n-1);
 }
+
+int FactorialIdeal(int n)
+{
+	if (n == 0) return 1;
+	else return n * FactorialIdeal(n - 1);
+	// n==0?1:FactorialIkeal (n-1); 
+}
 int Pow(int base, int power)
 {
 	if (power == 0) return powResult; 
 	powResult *=base;
 	Pow(base, power - 1);
 }
-void Fibonachi(int limitFibonachi)
+int Power(int base, int power)
 {
-	if (numFibonachi >= limitFibonachi) return;
-	
-	cout << numFibonachi << tab; 
-
-	Fibonachi(limitFibonachi);
+	if (power == 0) return 1;
+	else return base * Power(base, power - 1);
 }
+//void Fibonacсi(int n, int a = 0, int b = 1)
+//{
+//	return;
+//}
