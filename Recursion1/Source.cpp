62acdf9dfd68213d53f897879075c881ca709894
KonstantinSF <kosmos008@gmail.com>
2022-08-25 17:18:38 +0300

    Done, except power with minus value

--- Recursion1/Source.cpp
+++ Recursion1/Source.cpp
@@ -7,15 +7,15 @@ int FactorialIdeal(int n);
 //int Pow(int base, int power);
 double Power(int base, double power);
 int powResult = 1;
-void FibonacсiLimit(int limitFibonacсi, int a=0, int b=1);
-void FibonacсiNum(int Num, int i=0, int a = 0, int b = 1);
+void FibonacсiLimit(int limitFibonacсi, int a=0, int b=1);//отображение ряда Фибоначчи, числом меньше указанного
+void FibonacсiNum(int NumFibonacci, int i=0, long long a = 0, long long b = 1);//отображение ряда Фибоначчи, ограниченное количеством чисел
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	int n; 
 	int limitFibonacci;
-	int Num;
+	int NumFibonacci;
 	cout << "Введите число для вычисления факториала:"; cin>>n;
 	cout << "Значение факториала равно: " << Factorial(n) << endl; 
 	cout << "или другим способом: "<<FactorialIdeal(n) << endl;
@@ -26,9 +26,8 @@ void main()
 	cout << base << " в степени " << power << "= " << Power(base, power) << endl;
 	cout << "Введите предельное число для ряда Фибоначчи: "; cin >> limitFibonacci; 
 	FibonacсiLimit(limitFibonacci); cout << endl; 
-	cout << "Введите предельный номер числа из ряда Фибоначчи: "; cin >> Num; 
-	FibonacсiNum(Num); 
-
+	cout << "Введите предельный номер числа из ряда Фибоначчи: "; cin >> NumFibonacci;
+	FibonacсiNum(NumFibonacci);
 }
 
 int Factorial(int n)
@@ -40,7 +39,6 @@ int Factorial(int n)
 	factorial*= n; 
 	Factorial(n-1);
 }
-
 int FactorialIdeal(int n)
 {
 	if (n == 0) return 1;
@@ -60,15 +58,17 @@ double Power(int base, double power)
 	else return 1 / base * Power(base, power + 1);*/
 	return power == 0 ? 1 : power > 0? base * Power(base, power - 1) : 1 / base * Power(base, power + 1);
 }
+
 void FibonacсiLimit(int limitFibonacci, int a, int b)
 {
 	if (a> limitFibonacci) return;
 	cout << a << tab; 
 	FibonacсiLimit(limitFibonacci, b, a+b);
 }
-void FibonacсiNum(int Num, int i, int a, int b)
+void FibonacсiNum(int NumFibonacci, int i, long long a, long long b)
 {
-if (i>Num) return;
+	i++;
+if (i>NumFibonacci) return;
 cout << a << tab; 
-FibonacсiNum(Num, i++, b, a + b);
+FibonacсiNum(NumFibonacci, i, b, a + b);
 }
