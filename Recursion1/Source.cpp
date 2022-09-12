7f3ca20b2583b140937cb969f95e9cf0e51bbe0e
KonstantinSF <kosmos008@gmail.com>
2022-09-13 00:49:00 +0300

    fixed

--- Recursion1/Source.cpp
+++ Recursion1/Source.cpp
@@ -5,7 +5,7 @@ int Factorial(int n);
 int factorial = 1;
 int FactorialIdeal(int n);
 //int Pow(int base, int power);
-double Power(int base, double power);
+double Power(double base, double power);
 int powResult = 1;
 void FibonacсiLimit(int limitFibonacсi, int a=0, int b=1);//отображение ряда Фибоначчи, числом меньше указанного
 void FibonacсiNum(int NumFibonacci, int i=0, long long a = 0, long long b = 1);//отображение ряда Фибоначчи, ограниченное количеством чисел
@@ -19,7 +19,7 @@ void main()
 	cout << "Введите число для вычисления факториала:"; cin>>n;
 	cout << "Значение факториала равно: " << Factorial(n) << endl; 
 	cout << "или другим способом: "<<FactorialIdeal(n) << endl;
-	int base; int power; 
+	double base; double power; 
 	cout << "Введите значения основания степени: "; cin >> base;
 	cout << "Введите значение показателя степени: "; cin >> power; 
 	//cout << base << " в степени " << power << "=" << Pow(base, power) << endl;
@@ -51,7 +51,7 @@ int FactorialIdeal(int n)
 //	powResult *=base;
 //	Pow(base, power - 1);
 //}
-double Power(int base, double power)
+double Power(double base, double power)
 {
 	/*if (power == 0) return 1;
 	else if (power > 0) return base * Power(base, power - 1);
